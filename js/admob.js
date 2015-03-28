2e90d2604465dfa69054278937654d8eb0139e5d
hancoma <hancoma@gmail.com>
2015-03-28 16:37:17 +0900

    Revert "첫파일등록"
    This reverts commit bae0e6d425ae85b4e4fbaa72c970d8431f238325.

--- js/admob.js
+++
@@ -1,7 +0,0 @@
-   var admobid = {};
-   
-        admobid = {
-            banner: 'ca-app-pub-5159106620003574/7880797647', // or DFP format "/6253334/dfp_example_ad"
-            interstitial: 'ca-app-pub-5159106620003574/7880797647'
-        };
-   
